55f32909215f626512760234d98912b0d19ae092
Danila Suslov <assusdan@suslovd.ru>
2017-04-30 15:56:19 +0500

    Update compare_defconfig.py

--- compare_defconfig.py
+++ compare_defconfig.py
@@ -13,25 +13,25 @@ listb = []
 
 for line in a:
   if line.__len__()>3:
-	  if line[-1]!='\n': line+='\n'
-	  if line[0]!='#':
-		  lista.append(line[:-1].split('='))
+    if line[-1]!='\n': line+='\n'
+    if line[0]!='#':
+      lista.append(line[:-1].split('='))
 
 for line in b:
   if line.__len__()>3:
-  	if line[-1]!='\n': line+='\n'
-	  if line[0]!='#':
-		  listb.append(line[:-1].split('='))
+    if line[-1]!='\n': line+='\n'
+    if line[0]!='#':
+      listb.append(line[:-1].split('='))
 
 listd1 = [line for line in lista if line not in listb]
 listd2 = [line for line in listb if line not in lista]
 
 print ("ONLY IN FIRST: ")
 for line in listd1:
-	print(line)
+  print(line)
 
 print()
 
 print ("ONLY IN SECOND: ")
 for line in listd2:
-	print(line)
+  print(line)
