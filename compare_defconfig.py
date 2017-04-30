7f11ae82a937fd0f33ff70e6ed9a0734e2471999
Danila Suslov <assusdan@suslovd.ru>
2017-04-30 15:52:36 +0500

    Create compare_defconfig.py

---
+++ compare_defconfig.py
@@ -0,0 +1,37 @@
+# Usage: 
+# python compare_defconfig.py defconfigA defconfigB > difference.txt
+# 
+# Compare two defconfigs and add difference in file
+
+import sys
+
+a = open(sys.argv[1])
+b = open(sys.argv[2])
+
+lista = []
+listb = []
+
+for line in a:
+  if line.__len__()>3:
+	  if line[-1]!='\n': line+='\n'
+	  if line[0]!='#':
+		  lista.append(line[:-1].split('='))
+
+for line in b:
+  if line.__len__()>3:
+  	if line[-1]!='\n': line+='\n'
+	  if line[0]!='#':
+		  listb.append(line[:-1].split('='))
+
+listd1 = [line for line in lista if line not in listb]
+listd2 = [line for line in listb if line not in lista]
+
+print ("ONLY IN FIRST: ")
+for line in listd1:
+	print(line)
+
+print()
+
+print ("ONLY IN SECOND: ")
+for line in listd2:
+	print(line)
